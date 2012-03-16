GNOME/libnotify

Use notify-features.h instead of features.h

libnotify/notify.h
@@ -26,7 +26,7 @@
 
 #include <libnotify/notification.h>
 #include <libnotify/notify-enum-types.h>
-#include <libnotify/features.h>
+#include <libnotify/notify-features.h>
 
 G_BEGIN_DECLS
 

mate-desktop-legacy-archive/libmatenotify

libmatenotify/notify.h
@@ -27,7 +27,7 @@
 
 #include <libmatenotify/notification.h>
 #include <libmatenotify/notify-enum-types.h>
-#include <libmatenotify/features.h>
+#include <libmatenotify/notify-features.h>
 
 #ifdef __cplusplus
 extern "C" {