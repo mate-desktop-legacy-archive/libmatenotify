GNOME/libnotify

Add a macro for determining the version of the library

libnotify/notify.h
@@ -27,6 +27,7 @@
 
 #include <libnotify/notification.h>
 #include <libnotify/notify-enum-types.h>
+#include <libnotify/features.h>
 
 G_BEGIN_DECLS
 

mate-desktop-legacy-archive/libmatenotify

libmatenotify/notify.h
@@ -27,6 +27,7 @@
 
 #include <libmatenotify/notification.h>
 #include <libmatenotify/notify-enum-types.h>
+#include <libmatenotify/features.h>
 
 #ifdef __cplusplus
 extern "C" {